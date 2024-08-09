77198e7c90129a6fd27b47d4b47872954e5a73bc
nxc <nixiaoche@163.com>
2024-08-09 10:29:10 +0800

    fix: 修改启停任务接口权限

--- common/src/main/java/com/hcc/common/config/SecurityConfig.java
+++ common/src/main/java/com/hcc/common/config/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig {
                         .requestMatchers("/event/listEvent").permitAll()
                         .requestMatchers("/task/updateScore").permitAll()
                         .requestMatchers("/team/listTeamByParadigm").permitAll()
+                        .requestMatchers("/team/execAllTaskForFinals").permitAll()
+                        .requestMatchers("/team/stopAllTaskForFinals").permitAll()
                         .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
